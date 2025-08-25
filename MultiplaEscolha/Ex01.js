6afa5fbb22a526eef067a7271f3a8361662218a6
nieckasz <165843243+nieckasz@users.noreply.github.com>
2025-08-25 17:12:03 -0300

    Adição Da atividade Switch

---
+++ MultiplaEscolha/Ex01.js
@@ -0,0 +1,39 @@
+var dia
+
+dia=parseInt(prompt("Digite o correspondente ao dia da semana: "))
+
+switch(dia)
+{
+    case 1:{
+        alert("Domingo")
+        break
+    }
+    case 2:{
+        alert("Segunda")
+        break
+    }
+    case 3:{
+        alert("Terça")
+        break
+    }
+    case 4:{
+        alert("Quarta")
+        break
+    }
+    case 5:{
+        alert("Quinta")
+        break
+    }
+    case 6:{
+        alert("Sexta")
+        break
+    }
+    case 7:{
+        alert("Sábado")
+        break
+    }
+    default:{
+        alert("Opção Invalida")
+        break
+    }
+}
