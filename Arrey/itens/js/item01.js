f556d2435ad5d95de20de77ebf34c19a86d29a1a
RhuanSantosIF <rhuan.santos.ifpr@gmail.com>
2025-09-29 16:22:35 -0300

    Update item01.js

--- Arrey/itens/js/item01.js
+++ Arrey/itens/js/item01.js
@@ -1,3 +1,13 @@
 var a = [6,5,8,3,7]
 
+alert(a)/*Exibe todo o Array */
+
+alert(a[2])/*eXIBE APENAS UMA POSIÇÃO*/
+
+a[1]=4/*Altera uma elemento do array*/
 alert(a)
+
+/*percorrend o array usando laçode repetiçao*/ 
+for(var i=0;i<5;i++){
+    document.write("<h2>"+a[i]+"</h2>")
+}
